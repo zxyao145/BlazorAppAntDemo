75a24f70630b769087f1cd2e5159396f440e6b5a
vadimffe <vadim.ffegmail.com>
2021-12-13 22:44:23 +0200

    Image upload added

---
+++ BlazorAppAntDemo/Models/FileData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorAppAntDemo.Models
+{
+  public class SaveFile
+  {
+    public List<FileData> Files { get; set; }
+  }
+  public class FileData
+  {
+    public byte[] Data { get; set; }
+    public string FileType { get; set; }
+    public long Size { get; set; }
+  }
+}
--- BlazorAppAntDemo/Pages/Record.razor.cs
+++ BlazorAppAntDemo/Pages/Record.razor.cs
@@ -1,16 +1,26 @@
 ﻿using AntDesign;
 using BlazorAppAntDemo.Models;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
 
 namespace BlazorAppAntDemo.Pages
 {
   public partial class Record
   {
+    [Inject]
+    private MessageService Message { get; set; }
     private string ExpandIconPosition { get; set; }
     private int i = 1;
     private List<string> CategoryList { get; set; } 
     private string selectedCategoryValue;
     private List<Personell> PersonellData { get; set; }
+    private List<string> ImgUrls { get; set; }
+    private List<FileData> FileData { get; set; }
+
+    private string imageUrl;
+    private int maxFileSize;
+    private byte[] profilePictureByteArray;
+    private bool loading;
 
     [Parameter]
     public string Title { get; set; }
@@ -25,6 +35,9 @@ namespace BlazorAppAntDemo.Pages
     {
       this.ExpandIconPosition = "left";
       this.PersonellData = new List<Personell>();
+      this.ImgUrls = new List<string>();
+      this.FileData = new List<FileData>();
+      this.maxFileSize = 400000;
     }
 
 
@@ -64,6 +77,37 @@ namespace BlazorAppAntDemo.Pages
     private void Callback(string[] keys)
     {
 
+    }
+
+    private async Task Error(string message)
+    {
+      await Message.Error(message, 5);
+    }
+
+    private async Task OnFileSelection(InputFileChangeEventArgs e)
+    {
+      foreach (IBrowserFile file in e.GetMultipleFiles(this.maxFileSize))
+      {
+        if (file.Size < this.maxFileSize)
+        {
+          IBrowserFile imgFile = file;
+          this.profilePictureByteArray = new byte[imgFile.Size];
+          await imgFile.OpenReadStream().ReadAsync(profilePictureByteArray);
+          string imageType = imgFile.ContentType;
+          this.imageUrl = $"data:{imageType};base64,{Convert.ToBase64String(profilePictureByteArray)}";
+          this.ImgUrls.Add(imageUrl);
+          this.FileData.Add(new FileData
+          {
+            Data = profilePictureByteArray,
+            FileType = imageType,
+            Size = imgFile.Size
+          });
+        }
+        else
+        {
+          await this.Error("File is too big!");
+        }
+      }
     }
   }
 }
